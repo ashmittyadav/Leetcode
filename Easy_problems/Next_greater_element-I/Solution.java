258624f9ad6c4766bf4f3753638e277409c1da37
ashister <ashmityadavat92@gmail.com>
2026-07-21 23:41:05 +0530

    Leetcodde #876 problem - Next Greater Element-I

---
+++ Easy_problems/Next_greater_element-I/Solution.java
@@ -0,0 +1,58 @@
+/*
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+ 
+
+Example 1:
+
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+Example 2:
+
+Input: nums1 = [2,4], nums2 = [1,2,3,4]
+Output: [3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+- 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+*/
+
+
+// package Next_greater_element-I;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums1 = {4,1,2};
+        int[] nums2 = {1,3,4,2};
+        int[] ans = new int[10001];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= nums2[i]) {
+                st.pop();
+            }
+            ans[nums2[i]] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums2[i]);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = ans[nums1[i]];
+        }
+        System.out.println(nums1);
+        // return nums1;
+    }
+}
