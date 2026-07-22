bc03ef156fe314b3d6e36e88f792db4b45a1798e
ashister <ashmityadavat92@gmail.com>
2026-07-22 22:09:36 +0530

    Leetcode #796 problem - Rotate String

---
+++ Easy_problems/Rotate_string/Solution.java
@@ -0,0 +1,34 @@
+/*
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+*/
+
+package Rotate_string;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+        if(s.length() != goal.length()) {
+            System.out.println("false");
+        }
+        String concatenated = s + s;
+        if(concatenated.contains(goal)) {
+            System.out.println("true");
+        }
+        else System.out.println("false");
+    }
+}
