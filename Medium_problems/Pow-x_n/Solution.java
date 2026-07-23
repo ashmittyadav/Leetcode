d2466a912923eec3b8e0afc0b98db54549b9b742
ashister <ashmityadavat92@gmail.com>
2026-07-23 19:48:15 +0530

    Leetcode #50 problem - Pow(x,n)

---
+++ Medium_problems/Pow-x_n/Solution.java
@@ -0,0 +1,36 @@
+/*
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+
+
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+
+
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+*/
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+        double x = 2.00000;
+        int n = 10;
+
+        double res = Math.pow(x, n);
+        System.out.println(res);
+    }
+}
+
