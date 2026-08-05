318d3141aa0ed59682659bc9fdb01a0d0dd14db9
ashister <ashmityadavat92@gmail.com>
2026-08-05 23:45:08 +0530

    Leetcode #3731 problem - Find Missing Elements

---
+++ Easy_problems/FindMissingElements/Solution.java
@@ -0,0 +1,60 @@
+/*
+You are given an integer array nums consisting of unique integers.
+
+Originally, nums contained every integer within a certain range. However, some integers might have gone missing from the array.
+
+The smallest and largest integers of the original range are still present in nums.
+
+Return a sorted list of all the missing integers in this range. If no integers are missing, return an empty list.
+
+ 
+
+Example 1:
+
+Input: nums = [1,4,2,5]
+
+Output: [3]
+
+Explanation:
+
+The smallest integer is 1 and the largest is 5, so the full range should be [1,2,3,4,5]. Among these, only 3 is missing.
+
+Example 2:
+
+Input: nums = [7,8,6,9]
+
+Output: []
+
+Explanation:
+
+The smallest integer is 6 and the largest is 9, so the full range is [6,7,8,9]. All integers are already present, so no integer is missing.
+
+Example 3:
+
+Input: nums = [5,1]
+
+Output: [2,3,4]
+
+Explanation:
+
+The smallest integer is 1 and the largest is 5, so the full range should be [1,2,3,4,5]. The missing integers are 2, 3, and 4.
+*/
+
+import java.util.Vector;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        Arrays.sort(nums);
+        int temp = 0;
+        int left = nums[0];
+        int right = nums[nums.length - 1];
+        Vector<Integer> arr = new Vector<>();
+        for (int i = left; i <= right; i++) {
+            if (nums[temp] != i)
+                arr.add(i);
+            else
+                temp++;
+        }
+        return arr;
+    }
+}
