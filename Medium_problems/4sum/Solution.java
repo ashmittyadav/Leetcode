58fea6e9c698604bbb0c81093fd531b31cb793bc
ashister <ashmityadavat92@gmail.com>
2026-08-07 22:45:03 +0530

    Leetcode #18 problem - 4sum

---
+++ Medium_problems/4sum/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        int[] nums = {1,0,-1,0,-2,2};
+        int target = 0;
+        int n = nums.length;
+        int k = 0;
+        Arrays.sort(nums);
+        for(int i = 0 ; i < n - 3; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            for(int j = i + 1; j < n - 2; j++) {
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left  = j + 1;
+                int right = n - 1;
+                while(left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum == target) {
+                        result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        left++;
+                        right--;
+                        while(left < right && nums[left] == nums[left - 1]) left++;
+                        while(left < right && nums[right] == nums[right + 1]) right--;
+                    }
+                    else if(sum < target) left++;
+                    else right--;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+
+}
