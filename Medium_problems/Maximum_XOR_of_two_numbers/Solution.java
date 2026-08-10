70858bd90319f48dae31d2dc1d2b85028e3444dc
ashister <ashmityadavat92@gmail.com>
2026-08-10 23:11:02 +0530

    Leetcode problem - Maximum XOR of two elements

---
+++ Medium_problems/Maximum_XOR_of_two_numbers/Solution.java
@@ -0,0 +1,33 @@
+/*
+Given an integer array nums, return the maximum result of nums[i] XOR nums[j], where 0 <= i <= j < n.
+
+Example 1:
+
+Input: nums = [3,10,5,25,2,8]
+Output: 28
+Explanation: The maximum result is 5 XOR 25 = 28.
+
+Example 2:
+
+Input: nums = [14,70,53,83,49,91,36,80,92,51,66,70]
+Output: 127
+*/
+
+package Medium_problems.Maximum_XOR_of_two_numbers;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {14,70,53,83,49,91,36,80,92,51,66,70};
+        int largest = 0;
+        int i = nums[0];
+        int j = 0;
+        while(i != )
+            //     int max = nums[i] ^ nums[j];
+            //     if(largest < max) {
+            //         largest = max;
+            //     }
+            }
+        }        
+        // System.out.println(largest);
+    }
+}
